a6edf0717c65afea54dde4b69993bb1a450a45ce
lucciii33 <angelomaiele@gmail.com>
2022-02-01 02:42:59 +0000

    simple counter code

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,32 @@
 import React from "react";
+import propTypes from "prop-types";
+import reactDom from "react-dom";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+//create your first compostring
+const Home = ({ counter }) => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container  justify-content-center row">
+			<div className="bg-dark text-white d-inline-flex">
+				<div className="icon col-1 mx-2">
+					<i className="far fa-clock"></i>
+				</div>
+				<div className="col-2 mx-2">{counter.num1}</div>
+				<div className="col-2 mx-2">{counter.num2}</div>
+				<div className="col-2 mx-2">{counter.num3}</div>
+				<div className="col-2 mx-2">{counter.num4}</div>
+				<div className="col-2 mx-2">{counter.num5}</div>
+				<div className="col-1 mx-2">{counter.num6}</div>
+			</div>
 		</div>
 	);
+};
+Home.propTypes = {
+	num1: propTypes.string,
+	num2: propTypes.string,
+	num3: propTypes.string,
+	num4: propTypes.string,
+	num5: propTypes.string,
+	num6: propTypes.string,
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,24 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+var seconds = 0;
+
+const SecondCounter = () => {
+	counter;
+	seconds += 1;
+	let secondString = "000000" + seconds.toString();
+	secondString = secondString.slice(-6);
+	var counter = {
+		num1: secondString.charAt(0),
+		num2: secondString.charAt(1),
+		num3: secondString.charAt(2),
+		num4: secondString.charAt(3),
+		num5: secondString.charAt(4),
+		num6: secondString.charAt(5),
+	};
+
+	ReactDOM.render(<Home counter={counter} />, document.querySelector("#app"));
+};
+
+setInterval(SecondCounter, 1000);
